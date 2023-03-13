7d4f9506a69818e2dc4bb4ca09b1b191b294e9c0
pacokwon <haechank@gmail.com>
2023-03-13 14:45:17 +0000

    Add > before evaluations

--- tylam/main.ml
+++ tylam/main.ml
@@ -5,21 +5,20 @@ let _interactive () =
         let lexbuf = Lexing.from_channel stdin in
         while true do
             let tterm = Parser.main Lexer.token lexbuf in
+            Printf.printf "> ";
             Syntax.print_typed_term tterm;
             print_newline ();
+            Printf.printf "> ";
             Syntax.typecheck tterm |> ignore;
             let debruijn = tterm
             |> Syntax.erase
             |> Syntax.to_debruijn in
             Syntax.print_debruijn debruijn;
             print_newline ();
+            Printf.printf "> ";
             debruijn
             |> Syntax.evaluate
             |> Syntax.print_debruijn;
-            (* let debruijn = named |> Syntax.to_debruijn in *)
-            (* named |> Syntax.printtm; *)
-            (* print_newline (); *)
-            (* debruijn |> Syntax.evaluate |> Syntax.print_debruijn; *)
             print_newline ();
         done
     with
